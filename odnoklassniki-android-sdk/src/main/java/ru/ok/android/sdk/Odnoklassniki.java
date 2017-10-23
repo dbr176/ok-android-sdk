af55f91937c766d9592cb01899d7cd2345c141a0
Alexey Goncharov <darin100@yandex.com>
2017-10-23 14:16:06 +0300

    Закомментировал проверку платформы

--- odnoklassniki-android-sdk/src/main/java/ru/ok/android/sdk/Odnoklassniki.java
+++ odnoklassniki-android-sdk/src/main/java/ru/ok/android/sdk/Odnoklassniki.java
@@ -259,13 +259,13 @@ public class Odnoklassniki {
         }
         requestParams.put(Shared.PARAM_APP_KEY, mAppKey);
         requestParams.put(Shared.PARAM_METHOD, method);
-        requestParams.put(Shared.PARAM_PLATFORM, Shared.APP_PLATFORM);
-        if (mode.contains(OkRequestMode.SDK_SESSION)) {
-            if (TextUtils.isEmpty(sdkToken)) {
-                throw new IllegalArgumentException("SDK token is required for method call, have not forget to call sdkInit?");
-            }
-            requestParams.put(Shared.PARAM_SDK_TOKEN, sdkToken);
-        }
+        //requestParams.put(Shared.PARAM_PLATFORM, Shared.APP_PLATFORM);
+        //if (mode.contains(OkRequestMode.SDK_SESSION)) {
+        //    if (TextUtils.isEmpty(sdkToken)) {
+        //        throw new IllegalArgumentException("SDK token is required for method call, have not forget to call sdkInit?");
+        //    }
+        //    requestParams.put(Shared.PARAM_SDK_TOKEN, sdkToken);
+        //}
         if (mode.contains(OkRequestMode.SIGNED)) {
             signParameters(requestParams);
             requestParams.put(Shared.PARAM_ACCESS_TOKEN, mAccessToken);
